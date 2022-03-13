a1af0e940ea70a7e6109ff3f54cf84ab6540338c
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-13 18:55:41 +0000

    create list for cards to be looped through

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# TO RUN CODE: python3 run.py
+
+# The Jack/Queen/King all count as 10
+# The Ace can count as 11 or 1
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
