faa1428979d2c7881a837a499997bfb6cb3ef075
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-14 22:04:10 +0000

    import random method to generate random cards

--- run.py
+++ run.py
@@ -6,6 +6,27 @@
 
 # The Jack/Queen/King all count as 10
 # The Ace can count as 11 or 1
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
 
-random_card():
+
+def random_card():
+    '''
+    FUNCTION: Will loop through available cards and assign a card to player
+    '''
+    all_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    chosen_card = random.choice(all_cards)
+    return chosen_card
+
+
+# Deal the user and computer 2 cards each
+user_cards = []
+
+# This for loop will run twice 'range(2)'
+# Assign new card = to out put of random
+for _ in range(2):
+    new_card = random_card()
+    user_cards += new_card
+
+
+# --- BUGS ----
