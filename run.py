57791f4522d7484e24ef3ac5f04a33b2dce5069c
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-16 21:07:58 +0000

    logic for place bet function appears to work

--- run.py
+++ run.py
@@ -18,6 +18,15 @@ import random
 user_cards = []
 com_cards = []
 game_over = False
+START_BET = 30
+BET_20 = 20
+BET_40 = 40
+BET_80 = 80
+player_bet = 0
+dealer_bet = None
+betting_over = False
+bets_placed = 0
+game_over = False
 
 
 def random_card():
@@ -28,6 +37,43 @@ def random_card():
     all_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     chosen_card = random.choice(all_cards)
     return chosen_card
+
+
+def place_bet():
+    '''
+    FUNCTION:
+    '''
+    global betting_over
+    global bets_placed
+    global player_bet
+    global dealer_bet
+    print("Betting always starts at €30")
+    
+    while not betting_over:
+        should_bet = input("Would you like to place bet? 'y' for yes,\
+'n' for no: ").lower()
+        if should_bet == "n":
+            betting_over = True
+        else:
+            player_bet = int(input("Please enter bet: 20, 40 or 80: "))
+        if player_bet == 20:
+            player_bet = BET_20 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+        if player_bet == 40:
+            player_bet = BET_40 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+        if player_bet == 80:
+            player_bet = BET_80 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+
+        return player_bet
+        return dealer_bet
 
 
 def calculate_card_sum(all_cards):
--- test.py
+++ test.py
@@ -1,25 +1,77 @@
+# TO RUN CODE: python3 test.py
+
 START_BET = 30
 BET_20 = 20
 BET_40 = 40
 BET_80 = 80
 player_bet = 0
-dealer_bet = player_bet
+dealer_bet = None
+betting_over = False
+bets_placed = 0
+game_over = False
 
-print("Betting always starts at €30")
-should_continue = input("Would you like to place bet? 'y' for yes,\
-'n' for no. \n").lower()
-player_bet = int(input("Please enter bet: 20, 40 or 80"))
 
-print("Betting always starts at €30")
-# should_continue = input("Would you like to place bet? 'y' for yes, 
-# 'n' for no. \n").lower()
-player_bet = int(input("Please enter bet: 20, 40 or 80"))
-if player_bet == 20:
-    player_bet = BET_20 + START_BET
-    print(player_bet)
-elif player_bet == 40:
-    player_bet = BET_40 + START_BET
-    print(player_bet)
-elif player_bet == 40:
-    player_bet = BET_80 + START_BET
-    print(player_bet)
+def place_bet():
+    '''
+    FUNCTION:
+    '''
+    global betting_over
+    global bets_placed
+    global player_bet
+    global dealer_bet
+    print("Betting always starts at €30")
+    
+    while not betting_over:
+        should_bet = input("Would you like to place bet? 'y' for yes,\
+'n' for no: ").lower()
+        if should_bet == "n":
+            betting_over = True
+        else:
+            player_bet = int(input("Please enter bet: 20, 40 or 80: "))
+        if player_bet == 20:
+            player_bet = BET_20 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+        if player_bet == 40:
+            player_bet = BET_40 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+        if player_bet == 80:
+            player_bet = BET_80 + START_BET
+            dealer_bet = player_bet
+            print(f" Player bet this hand: €{player_bet}")
+            print(f" Dealer bet this hand: €{dealer_bet}")
+
+        return player_bet
+        return dealer_bet
+
+
+# def display_bet():
+#     '''
+#     FUNCTION:
+#     '''
+#     # global MAX_BET
+#     # global player_bet
+#     player_bet = int(input("Please enter bet: 20, 40 or 80: "))
+
+#     if player_bet == 20:
+#         player_bet = BET_20 + START_BET
+#         dealer_bet = player_bet
+#         print(f" Player bet this hand: €{player_bet}")
+#         print(f" Dealer bet this hand: €{dealer_bet}")
+#     elif player_bet == 40:
+#         player_bet = BET_40 + START_BET
+#         dealer_bet = player_bet
+#         print(f" Player bet this hand: €{player_bet}")
+#         print(f" Dealer bet this hand: €{dealer_bet}")
+#     elif player_bet == 80:
+#         player_bet = BET_80 + START_BET
+#         dealer_bet = player_bet
+#         print(f" Player bet this hand: €{player_bet}")
+#         print(f" Dealer bet this hand: €{dealer_bet}")
+
+
+place_bet()
+# display_bet()
