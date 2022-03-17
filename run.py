e63c969b2c92dc2258cfb0c6b16b5f3cb1009b10
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-17 15:01:17 +0000

    change place bet calculation

--- run.py
+++ run.py
@@ -11,11 +11,21 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 The cards in the list have equal probability of being drawn.
 Cards are not removed from the deck as they are drawn.'''
 
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# TO RUN CODE: python3 run.py
+
+'''
+The deck is unlimited in size.
+There are no jokers.
+The Jack/Queen/King all count as 10.
+The the Ace can count as 11 or 1.
+Use the following list as the deck of cards:
+[11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+The cards in the list have equal probability of being drawn.
+Cards are not removed from the deck as they are drawn.
+'''
+
 import random
-# from game_visuals import card_art
-# import rules from gamerules
-# Need to use clear function (os system)
-# from art import logo
 
 # GLOBAL VARIABLES
 user_cards = []
@@ -28,18 +38,18 @@ BET_20 = 20
 BET_40 = 40
 BET_80 = 80
 player_bet = 0
-dealer_bet = None
+dealer_bet = 0
 betting_over = False
 is_game_over = False
 round_pot = 0
-player_pot = 500
-dealer_pot = 500
+player_pot = 1000
+dealer_pot = 1000
 
 
 def random_card():
     '''
     FUNCTION: Will loop through available cards
-    and assign a random card to player
+    and then return a randomly selected one from the list
     '''
     all_cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     chosen_card = random.choice(all_cards)
@@ -48,15 +58,15 @@ def random_card():
 
 def place_bet():
     '''
-    FUNCTION:
+    FUNCTION: Ask player to place bet from 3 options: 20, 40, 80
+    Dealer will match this bet and both bets are added to the pot,
+    for that round
     '''
-    global betting_over
-    global bets_placed
-    global player_bet
-    global dealer_bet
-    global round_pot
+    betting_over = False
+    player_bet = 0
+    dealer_bet = 0
+    round_pot = 0
     print("Betting always starts at €30")
-    
     while not betting_over:
         should_bet = input("Would you like to place bet? 'y' for yes,\
 'n' for no: ").lower()
@@ -107,25 +117,21 @@ def calculate_card_sum(all_cards):
     FUNCTION: Take list of both hands (user and com)
     and return the sum of those cards
     '''
-    # CODE: if sum(cards) == 21 and len(cards) == 2:
-    if 11 in all_cards and 10 in all_cards and len(all_cards) == 2:
+    if sum(all_cards) == 21 and len(all_cards) == 2:
         return 0
-   
     # Check for an 11 (ace). If the score is already over 21:
     # remove the 11 and replace it with a 1
     if 11 in all_cards and sum(all_cards) > 21:
         all_cards.remove(11)
         all_cards.append(1)
-    
     return sum(all_cards)
 
 
-# pass in the user_score and computer_score. If the computer and user
-# both have the same score, then it's a draw. If the computer has
-# a blackjack (0), then the user loses. If the user has a blackjack (0),
-# then the user wins. If the user_score is over 21, then the user
-# (and vice versa). If none of the above, then the player with the 
-# highest score wins
+# pass in the user_hand and com_hand. If the computer and user
+# both have the same score, then it's a draw.
+# If the computer has a blackjack (0), then the user loses. (vice versa)
+# If the user_hand is over 21, then the user loses (vice versa)
+# If none of the above, then the player with the highest hand wins
 def compare_hands(user_hand, com_hand):
     '''
     FUNCTION: Compare values of user_hand and com_hand and determine the winner
@@ -184,49 +190,26 @@ def play_game():
     Deal random cards into player's and dealer's hands
     Check for end game considtion and ask if user wants to play again
     '''
-    global user_cards
-    global com_cards
-    global game_over
-    global user_hand
-    global com_hand
-    # print(logo)
-
-
-# This for loop will run twice with 'range(2)'
-# use append to have random_card function to output as a list
-    for i in range(2):
+    user_cards = []
+    com_cards = []
+    user_hand = 0
+    com_hand = 0
+    game_over = False
+    # This for loop will run twice with 'range(2)'
+    # use append to have 'random_card' FUNCTION to output as a list
+    for card in range(2):
         user_cards.append(random_card())
+        # print(card_art[user_hand])
         com_cards.append(random_card())
+        # print(card_art[com_hand])
 
-# The score will need to be rechecked with every new card drawn
-# and the checks in 'calculate_card_sum' need to be repeated 
-# until the game ends.
+    # The score will need to be rechecked with every new card drawn
+    # and the checks in 'calculate_card_sum' need to be repeated
+    # until the game ends.
     while not game_over:
-        # Call 'calculate_score'. If the computer or the user has a blackjack
-        # (0) or if the user's score is over 21, then the game ends.
-       
+        # Call 'calculate_card_sum'. If the computer or the user has a
+        # blackjack (0) or if the user's score is over 21, then the game ends.
         user_hand = calculate_card_sum(user_cards)
-        # if user_cards == 2:
-        #     print(card_art[1])
-        # elif user_cards == 3:
-        #     print(card_art[2])
-        # elif user_cards == 4:
-        #     print(card_art[3])
-        # elif user_cards == 5:
-        #     print(card_art[4])
-        # elif user_cards == 6:
-        #     print(card_art[5])
-        # elif user_cards == 7:
-        #     print(card_art[6])
-        # elif user_cards == 8:
-        #     print(card_art[7])
-        # elif user_cards == 9:
-        #     print(card_art[8])
-        # elif user_cards == 10:
-        #     print(card_art[9])
-        # else:
-        #     print(card_art[0])
-
         com_hand = calculate_card_sum(com_cards)
         print(f"Your hand: {user_cards} current score: {user_hand}")
         print(f"Dealers first card is: {com_cards[0]}")
@@ -254,11 +237,11 @@ again or 'n' to pass: ").lower()
     print(f"Your final hand: {user_cards}, final score: {user_hand}")
     print(f"Computer's final hand: {com_cards}, final score: {com_hand}")
     print(compare_hands(user_hand, com_hand))
-
-
-# Hint 14: Ask the user if they want to restart the game. 
-# If they answer yes: clear the console and start a new game of blackjack 
+# Hint 14: Ask the user if they want to restart the game.
+# If they answer yes: clear the console and start a new game of blackjack
 # and show the logo from art.py.
+
+
 while input("Do you want to start a new round of Blackjack? \
 Type 'y' or 'n': \n").lower() == "y":
     print("\n")
@@ -266,7 +249,16 @@ Type 'y' or 'n': \n").lower() == "y":
     com_cards = []
     place_bet()
     play_game()
-    # clear()
+
+
+# --- BUGS ---- \\
+# user_cards += new_card did not work, so had to use append to put random_card 
+# function to output as a list
+    # new_card = random_card()
+    # user_cards.append(new_card)
+    # FIX: user_cards.append = [random_card()]
+# Hand were not being cleared after each round and kept being appended to
+# player and dealer hands
 
 
 # --- BUGS ---- \\
