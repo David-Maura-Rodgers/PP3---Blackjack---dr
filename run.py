c0b0001eed0b5916e3c2cf2c4be631e9c7d052f3
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-17 12:23:10 +0000

    create compare hands function

--- run.py
+++ run.py
@@ -2,6 +2,15 @@
 # TO RUN CODE: python3 run.py
 
 
+'''The deck is unlimited in size. 
+There are no jokers. 
+The Jack/Queen/King all count as 10.
+The the Ace can count as 11 or 1.
+Use the following list as the deck of cards:
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+The cards in the list have equal probability of being drawn.
+Cards are not removed from the deck as they are drawn.'''
+
 import random
 # from game_visuals import card_art
 # import rules from gamerules
