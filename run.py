fad55c68e041e850d1a736303bda72cec24904fc
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-18 12:29:57 +0000

    create return winner function

--- run.py
+++ run.py
@@ -15,11 +15,11 @@ Cards are not removed from the deck as they are drawn.
 import random
 
 # GLOBAL VARIABLES
-user_cards = []
+player_cards = []
 com_cards = []
-user_hand = 0
+player_hand = 0
 com_hand = 0
-game_over = False
+
 START_BET = 30
 BET_20 = 20
 BET_40 = 40
@@ -27,7 +27,9 @@ BET_80 = 80
 player_bet = 0
 dealer_bet = 0
 betting_over = False
+round_over = False
 is_game_over = False
+
 round_pot = 0
 player_pot = 1000
 dealer_pot = 1000
@@ -101,7 +103,7 @@ def calculate_card_sum(all_cards):
     '''
     FUNCTION: check for a Blackjack:
     a hand with only 2 cards: ace + 10, and return 0
-    FUNCTION: Take list of both hands (user and com)
+    FUNCTION: Take list of both hands (player and com)
     and return the sum of those cards
     '''
     if sum(all_cards) == 21 and len(all_cards) == 2:
@@ -114,38 +116,39 @@ def calculate_card_sum(all_cards):
     return sum(all_cards)
 
 
-# pass in the user_hand and com_hand. If the computer and user
+# pass in the player_hand and com_hand. If the computer and player
 # both have the same score, then it's a draw.
-# If the computer has a blackjack (0), then the user loses. (vice versa)
-# If the user_hand is over 21, then the user loses (vice versa)
+# If the computer has a blackjack (0), then the player loses. (vice versa)
+# If the player_hand is over 21, then the player loses (vice versa)
 # If none of the above, then the player with the highest hand wins
-def compare_hands(user_hand, com_hand):
+def compare_hands(player_hand, com_hand):
     '''
-    FUNCTION: Compare values of user_hand and com_hand and determine the winner
+    FUNCTION: Compare values of player_hand and com_hand and determine 
+    the winner
     '''
-    if user_hand == com_hand:
+    if player_hand == com_hand:
         return "This round is a draw . . ."
     elif com_hand == 0:
         print(f"Player Pot is now: €{player_pot}")
         print(f"Dealer Pot is now: €{dealer_pot}")
         print("\n")
         return "You lose - dealer has Blackjack!!"
-    elif user_hand == 0:
+    elif player_hand == 0:
         print(f"Player Pot is now: €{player_pot}")
         print(f"Dealer Pot is now: €{dealer_pot}")
         print("\n")
         return "You Win - with a Blackjack 😎"
-    elif user_hand > 21:
+    elif player_hand > 21:
         print(f"Player Pot is now: €{player_pot}")
         print(f"Dealer Pot is now: €{dealer_pot}")
         print("\n")
         return "You went over. You lose 😭"
-    elif com_hand > 21:
+    elif player_hand > 21:
         print(f"Player Pot is now: €{player_pot}")
         print(f"Dealer Pot is now: €{dealer_pot}")
         print("\n")
         return "Opponent went over. You win 😁"
-    elif user_hand > com_hand:
+    elif player_hand > com_hand:
         print(f"Player Pot is now: €{player_pot}")
         print(f"Dealer Pot is now: €{dealer_pot}")
         print("\n")
@@ -161,30 +164,31 @@ def play_game():
     '''
     FUNCTION: Will start the game:
     Deal random cards into player's and dealer's hands
-    Check for end game condition and ask if user wants to play again
+    Check for end game condition and ask if player wants to play again
     '''
-    user_cards = []
+    player_cards = []
     com_cards = []
-    user_hand = 0
+    player_hand = 0
     com_hand = 0
-    game_over = False
+    round_over = False
     # This for loop will run twice with 'range(2)'
     # use append to have 'random_card' FUNCTION to output as a list
     for card in range(2):
-        user_cards.append(random_card())
-        # print(card_art[user_hand])
-        com_cards.append(random_card())
+        player_cards.append(random_card())
+        # print(card_art[player_hand])
+        player_cards.append(random_card())
         # print(card_art[com_hand])
 
     # The score will need to be rechecked with every new card drawn
     # and the checks in 'calculate_card_sum' need to be repeated
     # until the game ends.
-    while not game_over:
-        # Call 'calculate_card_sum'. If the computer or the user has a
-        # blackjack (0) or if the user's score is over 21, then the game ends.
-        user_hand = calculate_card_sum(user_cards)
+    while not round_over:
+        # Call 'calculate_card_sum'. If the computer or the player has a
+        # blackjack (0) or if the player's score is over 21, then the game 
+        # ends.
+        player_hand = calculate_card_sum(player_cards)
         com_hand = calculate_card_sum(com_cards)
-        print(f"Your hand: {user_cards} current score: {user_hand}")
+        print(f"Your hand: {player_cards} current score: {player_hand}")
         print(f"Dealers first card is: {com_cards[0]}")
         print("\n")
 
@@ -195,62 +199,62 @@ def play_game():
         if com_hand == 0:
             player_pot = player_pot - round_pot
             dealer_pot = dealer_pot + round_pot
-        elif user_hand == 0:
+        elif player_hand == 0:
             player_pot = player_pot + round_pot
             dealer_pot = dealer_pot - round_pot
-        elif user_hand > 21:
+        elif player_hand > 21:
             player_pot = player_pot - round_pot
             dealer_pot = dealer_pot + round_pot
         elif com_hand > 21:
             player_pot = player_pot + round_pot
             dealer_pot = dealer_pot - round_pot
-        elif user_hand > com_hand:
+        elif player_hand > com_hand:
             player_pot = player_pot + round_pot
             dealer_pot = dealer_pot - round_pot
         else:
             player_pot = player_pot - round_pot
             dealer_pot = dealer_pot + round_pot
 
-        if user_hand == 0 or com_hand == 0 or user_hand > 21:
-            game_over = True
+        if player_hand == 0 or com_hand == 0 or player_hand > 21:
+            round_over = True
         else:
-            user_deal_again = input("Type 'y' to deal \
+            player_deal_again = input("Type 'y' to deal \
 again or 'n' to pass: ").lower()
-            if user_deal_again == "y":
-                user_cards.append(random_card())
+            if player_deal_again == "y":
+                player_cards.append(random_card())
                 print(f"Dealer's hand: {com_cards} current score: {com_hand}")
             else:
-                game_over = True
+                round_over = True
 
 
-# Once the user is done, it's time to let the computer play.
+# Once the player is done, it's time to let the computer play.
 # The computer should keep drawing cards as long as it has
 # a score less than 17.
     while com_hand != 0 and com_hand < 17:
         com_cards.append(random_card())
         com_hand = calculate_card_sum(com_cards)
 
-    print(f"Your final hand: {user_cards}, final score: {user_hand}")
+    print(f"Your final hand: {player_cards}, final score: {player_hand}")
     print(f"Computer's final hand: {com_cards}, final score: {com_hand}")
-    print(compare_hands(user_hand, com_hand))
+    print(compare_hands(player_hand, com_hand))
 
 
-# Ask the user if they want to restart the game.
+# Ask the player if they want to restart the game.
 while input("Do you want to start a new round of Blackjack? \
 Type 'y' or 'n': \n").lower() == "y":
     print("\n")
-    user_cards = []
+    player_cards = []
     com_cards = []
     place_bet()
     play_game()
     
 
 # --- BUGS ---- \\
-# user_cards += new_card did not work, so had to use append to put random_card
-# function to output as a list
+# player_cards += new_card did not work, so had to use append to 
+# put random_card function to output as a list
     # new_card = random_card()
-    # user_cards.append(new_card)
-    # FIX: user_cards.append = [random_card()]
+    # player_cards.append(new_card)
+    # FIX: player_cards.append = [random_card()]
 # Hand were not being cleared after each round and kept being appended to
 # player and dealer hands
 
